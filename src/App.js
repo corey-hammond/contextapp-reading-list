d4a32b737bbcaa59d2e8fe3708b2e911e66969b9
Corey Hammond <coreyjhammond@gmail.com>
2021-02-11 14:36:18 -0800

    added context consumer without hooks

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
 import ThemeContextProvider from './context/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './context/AuthContext';
 
 function App() {
   return (
     <div className='App'>
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
