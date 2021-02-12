cbaa4aa1dae8b5ff0014c7f64e23417877af6f45
Corey Hammond <coreyjhammond@gmail.com>
2021-02-12 09:55:09 -0800

    more context/hooks demo, created booklist app

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,30 +1,27 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
+import { BookContext } from '../context/BookContext';
 import { ThemeContext } from '../context/ThemeContext';
 
-class BookList extends Component {
-  render() {
-    return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, light, dark } = context;
-          const theme = isLightTheme ? light : dark;
-
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className='book-list'
+      style={{ background: theme.bg, color: theme.syntax }}
+    >
+      <ul>
+        {books.map((book) => {
           return (
-            <div
-              className='book-list'
-              style={{ background: theme.bg, color: theme.syntax }}
-            >
-              <ul>
-                <li style={{ background: theme.ui }}>the way of kings</li>
-                <li style={{ background: theme.ui }}>the name of the wind</li>
-                <li style={{ background: theme.ui }}>the final empire</li>
-              </ul>
-            </div>
+            <li key={book.id} style={{ background: theme.ui }}>
+              {book.title}
+            </li>
           );
-        }}
-      </ThemeContext.Consumer>
-    );
-  }
-}
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
